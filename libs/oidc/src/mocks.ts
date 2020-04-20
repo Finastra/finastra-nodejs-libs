0d65bec7217bf7f481017cde0829bf9735d8da64
David <david.bocle@finastra.com>
2020-04-20 12:59:56 +0200

    test(oidc): unit test for module

--- libs/oidc/src/mocks.ts
+++ libs/oidc/src/mocks.ts
@@ -1,9 +1,16 @@
 import { OidcModuleOptions } from './interfaces';
+import { Issuer } from 'openid-client';
 
 export const MOCK_ISSUER = 'http://issuer.io';
+export const CLIENT_ID = '123';
+
+export const MOCK_ISSUER_INSTANCE = new Issuer({ issuer: MOCK_ISSUER });
+export const MOCK_CLIENT_INSTANCE = new MOCK_ISSUER_INSTANCE.Client({
+  client_id: CLIENT_ID,
+});
 
 export const MOCK_OIDC_MODULE_OPTIONS: OidcModuleOptions = {
-  clientId: '123',
+  clientId: CLIENT_ID,
   clientSecret: '456',
   issuer: MOCK_ISSUER,
   redirectUriLogin: 'bla',
