c35925c78d74e4c0c737db26fbd2e5f716b19509
David <david.bocle@finastra.com>
2020-06-10 10:47:32 +0200

    test(oidc): middleware unit tests

--- libs/oidc/src/middlewares/user.middleware.spec.ts
+++ libs/oidc/src/middlewares/user.middleware.spec.ts
@@ -1,11 +1,10 @@
-import { JWKS } from 'jose';
+import { JWKS, JWT } from 'jose';
 import { UserMiddleware } from './user.middleware';
 import { OidcHelpers } from '../utils';
 import { createMock } from '@golevelup/nestjs-testing';
 import { Request, Response } from 'express';
 import { MOCK_CLIENT_INSTANCE, MOCK_OIDC_MODULE_OPTIONS } from '../mocks';
 import { TestingModule, Test } from '@nestjs/testing';
-import { ConfigService } from '@nestjs/config';
 
 describe('User Middleware', () => {
   let middleware: UserMiddleware;
@@ -16,12 +15,6 @@ describe('User Middleware', () => {
     MOCK_OIDC_MODULE_OPTIONS,
   );
 
-  class mockConfigService {
-    get(value) {
-      return value;
-    }
-  }
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -30,20 +23,33 @@ describe('User Middleware', () => {
           provide: OidcHelpers,
           useValue: MockOidcHelpers,
         },
-        {
-          provide: ConfigService,
-          useClass: mockConfigService,
-        },
       ],
     }).compile();
 
     middleware = module.get<UserMiddleware>(UserMiddleware);
   });
 
+  it('should not do anything if no bearer', () => {
+    const req = createMock<Request>();
+    const res = createMock<Response>();
+    const next = jest.fn();
+    middleware.use(req, res, next);
+    expect(next).toHaveBeenCalled();
+  });
+
   it('should add user in request', () => {
     const req = createMock<Request>();
     const res = createMock<Response>();
-    const next = function() {};
-    expect(middleware.use(req, res, next)).toBeTruthy();
+    const next = jest.fn();
+    jest.spyOn(JWT, 'verify').mockReturnValue({
+      username: 'John Doe',
+    } as any);
+
+    const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`;
+    req.headers.authorization = `Bearer ${token}`;
+
+    middleware.use(req, res, next);
+    expect(next).toHaveBeenCalled();
+    expect(req.user['userinfo']).toBeTruthy();
   });
 });
