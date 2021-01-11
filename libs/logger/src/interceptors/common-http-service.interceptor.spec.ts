4c73921911bc8a697239620bf6d5776a25c7c0d8
David Boclé <bcldvd@users.noreply.github.com>
2021-01-11 12:03:21 +0100

    test(logger): test interceptor in graphQL context

--- libs/logger/src/interceptors/common-http-service.interceptor.spec.ts
+++ libs/logger/src/interceptors/common-http-service.interceptor.spec.ts
@@ -1,5 +1,6 @@
 import { createMock } from '@golevelup/nestjs-testing';
 import { CallHandler, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { of } from 'rxjs';
 import { OMSLogger } from '../oms/oms.logger.service';
 import { LoggingInterceptor } from './common-http-service.interceptor';
@@ -15,6 +16,29 @@ describe('CommonHTTPInterceptor', () => {
     it('t1', async done => {
       const mockExecutionContext = createMock<ExecutionContext>();
       const mockHandler = createMock<CallHandler>();
+
+      jest.spyOn(mockHandler, 'handle').mockReturnValue(of(null));
+      const spy = jest.spyOn(interceptor['logger'], 'log');
+
+      interceptor.intercept(mockExecutionContext, mockHandler).subscribe(() => {
+        expect(spy).toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('should intercept in graphQL context', async done => {
+      const mockExecutionContext = createMock<ExecutionContext>();
+      const mockHandler = createMock<CallHandler>();
+
+      mockExecutionContext['contextType'] = 'graphql';
+
+      jest.spyOn(GqlExecutionContext, 'create').mockReturnValue(
+        createMock<GqlExecutionContext>({
+          getContext: () => ({
+            req: {},
+          }),
+        }),
+      );
 
       jest.spyOn(mockHandler, 'handle').mockReturnValue(of(null));
       const spy = jest.spyOn(interceptor['logger'], 'log');
