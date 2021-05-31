46fc4695f0be23b1330a07f90b4a441622ec38a9
David Boclé <bcldvd@users.noreply.github.com>
2021-05-31 17:31:29 +0200

    Release oidc (#145)
    * build(deps): bump bson from 1.1.3 to 1.1.6 (#138)
    
    Bumps [bson](https://github.com/mongodb/js-bson) from 1.1.3 to 1.1.6.
    - [Release notes](https://github.com/mongodb/js-bson/releases)
    - [Changelog](https://github.com/mongodb/js-bson/blob/v1.1.6/HISTORY.md)
    - [Commits](https://github.com/mongodb/js-bson/compare/v1.1.3...v1.1.6)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * build(deps): bump hosted-git-info from 2.8.5 to 2.8.9 (#140)
    
    Bumps [hosted-git-info](https://github.com/npm/hosted-git-info) from 2.8.5 to 2.8.9.
    - [Release notes](https://github.com/npm/hosted-git-info/releases)
    - [Changelog](https://github.com/npm/hosted-git-info/blob/v2.8.9/CHANGELOG.md)
    - [Commits](https://github.com/npm/hosted-git-info/compare/v2.8.5...v2.8.9)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: update codecov-action version (#142)
    
    * fix(oidc): bump openid-client to supress normalize-url vulnerability (#144)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: FrancineO <francine.o@live.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { HttpLoggingInterceptor, OMSLogger } from '@finastra/nestjs-logger';
 import { setupSession, TokenGuard } from '@finastra/nestjs-oidc';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -15,6 +16,10 @@ async function bootstrap() {
 
   setupSession(app, 'test-app');
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, () => {
+    Logger.log('Listening at ' + process.env.ORIGIN, 'main');
+  });
 }
 bootstrap();
