da2779688d1e6bd8434df452c6f8b144d91763ac
frigui dorsaf <dorsafrigui@gmail.com>
2021-09-15 13:58:52 +0200

    refactoring

--- libs/oidc/src/guards/token.guard.ts
+++ libs/oidc/src/guards/token.guard.ts
@@ -9,7 +9,6 @@ export class TokenGuard implements CanActivate {
   constructor(private readonly reflector: Reflector, private oidcService: OidcService) { }
   async canActivate(context: ExecutionContext) {
     let request;
-    let prefix = "";
     if (context['contextType'] === 'graphql') {
       request = GqlExecutionContext.create(context).getContext().req;
     } else {
@@ -20,9 +19,7 @@ export class TokenGuard implements CanActivate {
     
     if (isPublic) return true;
 
-    if (params) {
-       prefix = params.tenantId && params.channelType ? `/${params.tenantId}/${params.channelType}` : '';
-    }
+    const prefix = params && params.tenantId && params.channelType ? `/${params.tenantId}/${params.channelType}` : '';
 
     if (request.isAuthenticated() && (!this.oidcService.isExpired(request.user['authTokens'].expiresAt) || (request.url === `${prefix}/refresh-token`))) {
         return true;
