62fd22e3a43083f155b751d8c1b9d9ac27c62acd
David Boclé <bcldvd@users.noreply.github.com>
2021-09-28 13:28:01 +0200

    session mongo specific unit tests

--- libs/oidc/src/utils/session/session-mongo.spec.ts
+++ libs/oidc/src/utils/session/session-mongo.spec.ts
@@ -1,10 +1,12 @@
 import { createMock } from '@golevelup/nestjs-testing';
 import { INestApplication } from '@nestjs/common';
+import MongoStore from 'connect-mongo';
 import { sessionMongo } from './session-mongo';
-const MongoStore = require('connect-mongo');
+
+jest.mock('express-session');
 
 describe('sessionMongo', () => {
-  xit('should be defined', () => {
+  it('should be defined', () => {
     const app = createMock<INestApplication>();
     const spy = jest.spyOn(app, 'use').mockReturnThis();
     jest.spyOn(MongoStore, 'create').mockReturnThis();
