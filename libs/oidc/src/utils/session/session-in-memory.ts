d3172d550f6a5efe290b895b04e0396fce9761ab
David Boclé <bcldvd@users.noreply.github.com>
2021-09-30 12:01:16 +0200

    refactor session options

--- libs/oidc/src/utils/session/session-in-memory.ts
+++ libs/oidc/src/utils/session/session-in-memory.ts
@@ -1,21 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import session from 'express-session';
 import passport from 'passport';
-import { v4 as uuid } from 'uuid';
+import { baseSession } from './base-session';
 
 export function sessionInMemory(app: INestApplication, name: string) {
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || uuid(), // to sign session id
-      resave: false, // will default to false in near future: https://github.com/expressjs/session#resave
-      saveUninitialized: false, // will default to false in near future: https://github.com/expressjs/session#saveuninitialized
-      rolling: true, // keep session alive
-      cookie: {
-        maxAge: 30 * 60 * 1000, // session expires in 1hr, refreshed by `rolling: true` option.
-        httpOnly: true, // so that cookie can't be accessed via client-side script
-        secure: false,
-      },
       name,
+      ...baseSession,
     }),
   );
   app.use(passport.initialize());
--- libs/oidc/src/utils/session/session-mongo.ts
+++ libs/oidc/src/utils/session/session-mongo.ts
@@ -2,21 +2,13 @@ import MongoStore from 'connect-mongo';
 import { ConnectMongoOptions } from 'connect-mongo/build/main/lib/MongoStore';
 import session from 'express-session';
 import passport from 'passport';
-import { v4 as uiid } from 'uuid';
+import { baseSession } from './base-session';
 
 export const sessionMongo = (app, name, options: ConnectMongoOptions) => {
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || uiid(), // to sign session id
-      resave: false, // will default to false in near future: https://github.com/expressjs/session#resave
-      saveUninitialized: false, // will default to false in near future: https://github.com/expressjs/session#saveuninitialized
-      rolling: true, // keep session alive
-      cookie: {
-        maxAge: 30 * 60 * 1000, // session expires in 1hr, refreshed by `rolling: true` option.
-        httpOnly: true, // so that cookie can't be accessed via client-side script
-        secure: false,
-      },
       name,
+      ...baseSession,
       store: MongoStore.create(options),
     }),
   );
