25f915dcf81e7f84bd2e47f12eb3e7147741758f
David Boclé <bcldvd@users.noreply.github.com>
2021-10-20 11:12:14 +0200

    Add top level export of `sessionMongo` back but use `require` to prevent typescript from requiring unwanted dependencies for project not using it. (#190)

--- libs/oidc/src/utils/index.ts
+++ libs/oidc/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './external-idps';
 export * from './merge-defaults.util';
+export * from './session';
 export * from './session.serializer';
 export * from './setup-session';
 export * from './user-info';
