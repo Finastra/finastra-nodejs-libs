a75c631a87119a0d18053622b95d862b017f2341
iprimak <61782254+iprimak@users.noreply.github.com>
2021-10-20 11:28:04 +0300

    Add  Cache-Control header to all entries

--- libs/oidc/src/controllers/auth-multitenant.controller.ts
+++ libs/oidc/src/controllers/auth-multitenant.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Next, Param, Req, Res } from '@nestjs/common';
+import { Controller, Get, Next, Param, Req, Res, Header } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { isAvailableRouteForMultitenant } from '../decorators';
 import { Public } from '../decorators/public.decorator';
@@ -10,18 +10,21 @@ export class AuthMultitenantController {
   constructor(public oidcService: OidcService) {}
 
   @Get('/user')
+  @Header('Cache-Control', 'no-store, max-age=0')
   user(@Req() req) {
     return req.user.userinfo;
   }
 
   @Public()
   @Get('/login')
+  @Header('Cache-Control', 'no-store, max-age=0')
   login(@Req() req: Request, @Res() res: Response, @Next() next: Function, @Param() params) {
     this.oidcService.login(req, res, next, params);
   }
 
   @Public()
   @Get('/logout')
+  @Header('Cache-Control', 'no-store, max-age=0')
   async logout(@Req() req: Request, @Res() res: Response, @Param() params) {
     this.oidcService.logout(req, res, params);
   }
@@ -34,6 +37,7 @@ export class AuthMultitenantController {
 
   @Public()
   @Get('/loggedout')
+  @Header('Cache-Control', 'no-store, max-age=0')
   loggedOut(@Req() req: Request, @Res() res: Response, @Param() params) {
     this.oidcService.loggedOut(req, res, params);
   }
