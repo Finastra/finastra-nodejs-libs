5b0f33e6bc50a6038531c892c99eb97a6e8fd01f
David Boclé <bcldvd@users.noreply.github.com>
2022-01-11 18:09:59 +0100

    fix(oidc): Remove signature from logged jwt

---
+++ libs/oidc/src/utils/jwt.spec.ts
@@ -0,0 +1,12 @@
+import { removeSignatureFromJwt } from './jwt';
+
+describe('removeSignatureFromJwt', () => {
+  it('should remove signature from jwt', () => {
+    const jwt =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    const signaturelessJwt = removeSignatureFromJwt(jwt);
+    expect(signaturelessJwt).toBe(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ',
+    );
+  });
+});
