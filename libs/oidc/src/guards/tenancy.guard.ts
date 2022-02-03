5bc4aa8ea34a191cfbb22beab2d8ca908f275c46
Francine Ong <francine.o@live.com>
2022-02-04 00:39:39 +0100

    fix(oidc): fix tests

--- libs/oidc/src/guards/tenancy.guard.ts
+++ libs/oidc/src/guards/tenancy.guard.ts
@@ -20,10 +20,10 @@ export class TenancyGuard implements CanActivate {
     if (typeof isMultitenant !== 'undefined' && isMultitenant !== this.oidcService.isMultitenant) {
       throw new NotFoundException();
     } else if (
-      req.params.tenantId === 'favicon.ico' ||
       ((typeof isMultitenant === 'undefined' || isMultitenant === this.oidcService.isMultitenant) && !req.user) ||
       !req.user.userinfo.channel ||
       !req.params.tenantId ||
+      req.params.tenantId === 'favicon.ico' ||
       (!this.oidcService.options.channelType && !req.params.channelType) ||
       (req.user.userinfo.channel &&
         req.user.userinfo.tenant &&
